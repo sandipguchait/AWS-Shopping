dc46f4cebe2b352ac4e6b87facb87692b74830a5
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-13 13:11:00 +0530

    setting up authenticattion with withAuthentication

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+
 
 // Bring in default Element React theme
 import "element-theme-default";
+
+//Configuring Amplify 
+Amplify.configure(aws_exports);
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
