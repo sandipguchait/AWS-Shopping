98114438b21d4408418b87379727a1ce353d8307
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-13 13:50:44 +0530

    customizing styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { withAuthenticator } from 'aws-amplify-react';
+import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react';
 
 class App extends React.Component {
   state = {};
@@ -10,4 +10,38 @@ class App extends React.Component {
   }
 }
 
-export default withAuthenticator(App);
+const theme = {
+  ...AmplifyTheme,
+  navBar: {
+    ...AmplifyTheme.navBar,
+    backgroundColor: "lightblue"
+  },
+  navItem: {
+    ...AmplifyTheme.navItem,
+    fontSize: "15px",
+    fontWeight: "normal",
+    fontFamily: "Montserrat",
+    fontStyle: "italic"
+  },
+  button: {
+    ...AmplifyTheme.button,
+    backgroundColor: "var(--lightAmazonOrange)"
+  },
+  sectionBody: {
+    ...AmplifyTheme.sectionBody,
+    padding: "5px"
+  },
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: "var(--squidInk)"
+  },
+  inputLabel: {
+    ...AmplifyTheme.inputLabel,
+    fontSize: "15px",
+    fontWeight: "bold",
+    fontFamily: "Montserrat",
+    fontStyle: "normal"
+  }
+};
+
+export default withAuthenticator(App, true, [], null, theme);
