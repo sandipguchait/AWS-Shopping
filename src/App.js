8d1af0e7114f29d5bad88e56c1892571c034d316
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-13 16:19:23 +0530

    chaning authentication fucntion to Authenticator and using HubCapsule to keep track of user login details

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import React from "react";
 import "./App.css";
-import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react';
+import { Auth, Hub } from 'aws-amplify';
+import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
 
 class App extends React.Component {
-  state = {};
+  state = {
+    user: null
+  };
+
+  // Check user logged In or not when component mounts
+  componentDidMount() {
+    this.getuserData();
+    Hub.listen('auth', this, 'onHubCapsule')
+  };
+
+  getuserData = async () => {
+    const user = await Auth.currentAuthenticatedUser()
+    user ? this.setState({ user }) : this.setState({ user: null })
+  };
+
+  // Keep track of user state if they click login or logout
+  onHubCapsule = capsule => {
+    switch(capsule.payload.event) {
+      case "signIn":
+        console.log('signed In')
+        this.getuserData()
+        break;
+      case "signUp":
+        console.log('sign up')
+        break;
+      case "signOut":
+        console.log('sign out')
+        this.setState({ user: null })
+        break;
+      default:
+       return;
+    }
+  }
 
   render() {
-    return <div>App</div>;
+    const { user } = this.state;
+
+    return !user ? (
+      <Authenticator  theme={theme}/> 
+    ) : <div>App</div>
   }
 }
 
@@ -44,4 +81,5 @@ const theme = {
   }
 };
 
-export default withAuthenticator(App, true, [], null, theme);
+// export default withAuthenticator(App, true, [], null, theme);
+export default App;
