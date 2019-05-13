cc8ac712559ce0c55107972d7195bf3d9e7ece83
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-13 20:06:26 +0530

    adding market name , tags and doing mutation on it

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import ProfilePage from './pages/ProfilePage';
 import MarketPage from "./pages/MarketPage";
 import NavBar from './components/Navbar';
 
+export const UserContext = React.createContext();
+
 class App extends React.Component {
   state = {
     user: null
@@ -51,20 +53,22 @@ class App extends React.Component {
     return !user ? (
       <Authenticator  theme={theme}/> 
     ) : (
-      <BrowserRouter>
-        <React.Fragment>
-          {/* Navbar */}
-          <NavBar user={user}/>
-          {/* Routes */}
-          <div className="app-container">
-            <Route exact path="/" component={HomePage} />
-            <Route path="/profile" component={ProfilePage} />
-            <Route path="/markets/:id" 
-                component={({ match }) => <MarketPage id={match.params.id} />} 
-            />
-          </div>
-        </React.Fragment>
-      </BrowserRouter>
+      <UserContext.Provider value={{ user }}>
+        <BrowserRouter>
+          <React.Fragment>
+            {/* Navbar */}
+            <NavBar user={user}/>
+            {/* Routes */}
+            <div className="app-container">
+              <Route exact path="/" component={HomePage} />
+              <Route path="/profile" component={ProfilePage} />
+              <Route path="/markets/:id" 
+                  component={({ match }) => <MarketPage id={match.params.id} />} 
+              />
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
+      </UserContext.Provider>
     )
   }
 }
--- src/components/NewMarket.js
+++ src/components/NewMarket.js
@@ -1,12 +1,115 @@
 import React from "react";
-// prettier-ignore
-// import { Form, Button, Dialog, Input, Select, Notification } from 'element-react'
+import { API, graphqlOperation } from 'aws-amplify';
+import { createMarket } from '../graphql/mutations';
+import { Form, Button, Dialog, Input, Select, Notification } from 'element-react';
+import { UserContext } from '../App';
 
 class NewMarket extends React.Component {
-  state = {};
+  state = {
+    name: '',
+    tags: ["Arts", "Technology", "Entertainment", "Education", "Music"],
+    selectedTags: [],
+    options: [],
+    addMarketDialog: false,
+  };
+
+  // Adding New Market data to AWS Database
+  handleAddmarket = async ( user ) => {
+    try {
+      this.setState({ addMarketDialog: false })
+      const input = {
+        name: this.state.name,
+        owner: user.username,
+        tags: this.state.selectedTags
+      };
+      const result = await API.graphql(graphqlOperation(createMarket, { input }))
+      console.log(result)
+      console.info(`Created market: id ${result.data.createMarket.id}`)
+      this.setState({ name: "", selectedTags: [] })
+    } catch(err) {
+        console.error('Error adding Market', err )
+        Notification.error({
+          title: "Error",
+          message: `${err.message || "Error adding market"}`
+        })
+    }
+  };
+
+  // Filtering Tags function 
+  handleFilterTags = (query) => {
+   const options = this.state.tags
+      .map(tag => ({ value: tag, label: tag }))
+      .filter(tag => tag.label.toLowerCase().includes(query.toLowerCase()))
+    this.setState({ options })
+  };
 
   render() {
-    return <div>NewMarket</div>
+    return (
+      <UserContext.Consumer>
+
+        {({ user }) => <React.Fragment>
+          <div className="market-header">
+            <div className="market-title">
+              Create Your <strong className="spacing">Market</strong>
+              <Button type="text" icon="plus" className="market-title-button" 
+                onClick={()=> this.setState({ addMarketDialog: true })}
+              />
+            </div>       
+          </div>
+
+          <Dialog
+            title="Create New Market"
+            visible={this.state.addMarketDialog}
+            onCancel={() => this.setState({ addMarketDialog: false })}
+            size="large"
+            customClass="dialog"
+          >
+            <Dialog.Body>
+              <Form labelPosition="top">
+                <Form.Item label="Add Market Name">
+                  <Input 
+                      placeholder="Market Name"
+                      trim={true}
+                      value={this.state.name}
+                      onChange={name => this.setState({ name })}
+                  />
+                </Form.Item>
+                <Form.Item label=" Add Tags">
+                  <Select 
+                    multiple={true}
+                    filterable={true}
+                    placeholder="Market Tags"
+                    onChange={selectedTags => this.setState({ selectedTags})}
+                    remoteMethod={this.handleFilterTags}
+                    remote={true}
+                  >
+                  {this.state.options.map( option => (
+                    <Select.Option 
+                        key={option.value}
+                        label={option.label}
+                        value={option.value}
+                    />
+                  ))}
+                  </Select>
+                </Form.Item>
+              </Form>
+            </Dialog.Body>
+            <Dialog.Footer>
+              <Button onClick={() => this.setState({ addMarketDialog: false })}>
+                Cancel
+              </Button>
+              <Button
+                type="primary"
+                disabled={!this.state.name}
+                onClick={() => this.handleAddmarket(user)}
+              >
+                Add
+              </Button>
+            </Dialog.Footer>
+          </Dialog>
+        </React.Fragment>}
+      </UserContext.Consumer>
+    )
   }
 }
 
