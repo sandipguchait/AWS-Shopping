1c2b928b8a43c70978affc1567f472fff1de4889
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-17 20:03:46 +0530

    adding lambda function api as back end and making post request to /charge with payment details and getting back token

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from 'react-stripe-checkout';
+import { API } from 'aws-amplify';
 // import { Notification, Message } from "element-react";
 
 const stripeConfig = {
@@ -8,8 +9,25 @@ const stripeConfig = {
 }
 
 const PayButton = ({ product, user }) => {
+  const handleCharge = async token => {
+    //Using Lambds function making backend a post request to '/charge'
+    //using post to post stripe payment data from frontend to backend -> posting
+  
+    try {
+     const result =  await API.post('orderlambda', '/charge', { 
+        body: {
+          token
+        }
+      })
+      console.log(result);
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
   return (
     <StripeCheckout 
+      token={handleCharge}
       email={user.attributes.email}
       name={product.description}
       amount={product.price}
