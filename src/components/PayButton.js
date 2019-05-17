1b64dd8af86b310c25665ebd0734c306cbd4d6f9
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-17 19:08:40 +0530

    configuring stripe and displaying paybutton

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,8 +1,25 @@
 import React from "react";
+import StripeCheckout from 'react-stripe-checkout';
 // import { Notification, Message } from "element-react";
 
-const PayButton = () => {
-  return <div>PayButton</div>;
+const stripeConfig = {
+  currency: "USD",
+  publishableAPIKey: "pk_test_QREVHSBFyM21NjYBhGpnef9U"
+}
+
+const PayButton = ({ product, user }) => {
+  return (
+    <StripeCheckout 
+      email={user.attributes.email}
+      name={product.description}
+      amount={product.price}
+      shippingAddress={product.shipped}
+      billingAddress={product.shipped}
+      currency={stripeConfig.currency}
+      locale="auto"
+      stripeKey={stripeConfig.publishableAPIKey}
+    />
+  )
 };
 
 export default PayButton;
--- src/components/Product.js
+++ src/components/Product.js
@@ -104,7 +104,7 @@ class Product extends React.Component {
                       ${convertCentsToDollars(product.price)}
                     </span>
                     {!isProductOwner && (
-                        <PayButton />
+                        <PayButton product={product} user={user} />
                     )}
                   </div>
                 </div>
