c7359be89caf1020d6b2e8d4285f85cd23558941
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-18 15:59:27 +0530

    completing the app

--- src/App.js
+++ src/App.js
@@ -1,103 +1,131 @@
 import React from "react";
-import "./App.css";
-import { API,graphqlOperation ,Auth, Hub } from 'aws-amplify';
-import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import  { getUser } from './graphql/queries';
-
-// Components
-import HomePage from './pages/HomePage';
-import ProfilePage from './pages/ProfilePage';
-import MarketPage from "./pages/MarketPage";
-import NavBar from './components/Navbar';
+import { API, graphqlOperation, Auth, Hub } from "aws-amplify";
+import { getUser } from "./graphql/queries";
 import { registerUser } from "./graphql/mutations";
+import { Authenticator, AmplifyTheme } from "aws-amplify-react";
+import { Router, Route } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import MarketPage from "./pages/MarketPage";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+export const history = createBrowserHistory();
 
 export const UserContext = React.createContext();
 
 class App extends React.Component {
   state = {
-    user: null
+    user: null,
+    userAttributes: null
   };
 
-  // Check user logged In or not when component mounts
   componentDidMount() {
-    this.getuserData();
-    Hub.listen('auth', this, 'onHubCapsule')
+    this.getUserData();
+    Hub.listen("auth", this, "onHubCapsule");
+  }
+
+  getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser();
+    user
+      ? this.setState({ user }, () => this.getUserAttributes(this.state.user))
+      : this.setState({ user: null });
   };
 
-  getuserData = async () => {
-    const user = await Auth.currentAuthenticatedUser()
-    user ? this.setState({ user }) : this.setState({ user: null })
+  getUserAttributes = async authUserData => {
+    const attributesArr = await Auth.userAttributes(authUserData);
+    const attributesObj = Auth.attributesToObject(attributesArr);
+    this.setState({ userAttributes: attributesObj });
   };
 
-  // Keep track of user state if they click login or logout
   onHubCapsule = capsule => {
-    switch(capsule.payload.event) {
+    switch (capsule.payload.event) {
       case "signIn":
-        console.log('signed In')
-        this.getuserData();
-        this.registerNewUser(capsule.payload.data)
+        console.log("signed in");
+        this.getUserData();
+        this.registerNewUser(capsule.payload.data);
         break;
       case "signUp":
-        console.log('sign up')
+        console.log("signed up");
         break;
       case "signOut":
-        console.log('sign out')
-        this.setState({ user: null })
+        console.log("signed out");
+        this.setState({ user: null });
         break;
       default:
-       return;
+        return;
     }
   };
 
-  // Registering new user and getting their details
   registerNewUser = async signInData => {
     const getUserInput = {
       id: signInData.signInUserSession.idToken.payload.sub
-    }
-    const { data } = await API.graphql(graphqlOperation( getUser, getUserInput ));
-    
-    //if there is no user or user hastneen registered before
-    if(!data.getUser) {
+    };
+    const { data } = await API.graphql(graphqlOperation(getUser, getUserInput));
+    // if we can't get a user (meaning the user hasn't been registered before), then we execute registerUser
+    if (!data.getUser) {
       try {
         const registerUserInput = {
           ...getUserInput,
           username: signInData.username,
           email: signInData.signInUserSession.idToken.payload.email,
           registered: true
-        }
-        const newUser = await API.graphql(graphqlOperation( registerUser, { input: registerUserInput }));
-        console.log(newUser)
-
-      } catch(err) {
+        };
+        const newUser = await API.graphql(
+          graphqlOperation(registerUser, { input: registerUserInput })
+        );
+        console.log({ newUser });
+      } catch (err) {
         console.error("Error registering new user", err);
       }
     }
-  }
+  };
+
+  handleSignout = async () => {
+    try {
+      await Auth.signOut();
+    } catch (err) {
+      console.error("Error signing out user", err);
+    }
+  };
 
   render() {
-    const { user } = this.state;
+    const { user, userAttributes } = this.state;
 
     return !user ? (
-      <Authenticator  theme={theme}/> 
+      <Authenticator theme={theme} />
     ) : (
-      <UserContext.Provider value={{ user }}>
-        <BrowserRouter>
-          <React.Fragment>
-            {/* Navbar */}
-            <NavBar user={user}/>
+      <UserContext.Provider value={{ user, userAttributes }}>
+        <Router history={history}>
+          <>
+            {/* Navigation */}
+            <Navbar user={user} handleSignout={this.handleSignout} />
+
             {/* Routes */}
             <div className="app-container">
               <Route exact path="/" component={HomePage} />
-              <Route path="/profile" component={() => <ProfilePage user={user} />} />
-              <Route path="/markets/:id" 
-                  component={({ match }) => <MarketPage user={user} id={match.params.id} />} 
+              <Route
+                path="/profile"
+                component={() => (
+                  <ProfilePage user={user} userAttributes={userAttributes} />
+                )}
+              />
+              <Route
+                path="/markets/:marketId"
+                component={({ match }) => (
+                  <MarketPage
+                    user={user}
+                    userAttributes={userAttributes}
+                    marketId={match.params.marketId}
+                  />
+                )}
               />
             </div>
-          </React.Fragment>
-        </BrowserRouter>
+          </>
+        </Router>
       </UserContext.Provider>
-    )
+    );
   }
 }
 
@@ -105,18 +133,11 @@ const theme = {
   ...AmplifyTheme,
   navBar: {
     ...AmplifyTheme.navBar,
-    backgroundColor: "lightblue"
-  },
-  navItem: {
-    ...AmplifyTheme.navItem,
-    fontSize: "15px",
-    fontWeight: "normal",
-    fontFamily: "Montserrat",
-    fontStyle: "italic"
+    backgroundColor: "#ffc0cb"
   },
   button: {
     ...AmplifyTheme.button,
-    backgroundColor: "var(--lightAmazonOrange)"
+    backgroundColor: "var(--amazonOrange)"
   },
   sectionBody: {
     ...AmplifyTheme.sectionBody,
@@ -125,13 +146,6 @@ const theme = {
   sectionHeader: {
     ...AmplifyTheme.sectionHeader,
     backgroundColor: "var(--squidInk)"
-  },
-  inputLabel: {
-    ...AmplifyTheme.inputLabel,
-    fontSize: "15px",
-    fontWeight: "bold",
-    fontFamily: "Montserrat",
-    fontStyle: "normal"
   }
 };
 
